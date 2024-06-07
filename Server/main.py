d9db37a44afa3a907c413d716f2d5c253f498177
E-O-Digas <guitoempireallias@gmail.com>
2024-06-07 18:04:44 -0300

    2° commit

--- Server/main.py
+++ Server/main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
+import model
 
 app = FastAPI()
 
 @app.get()
 async def root():
     return{"message", "aaaaaa"}
+
+alunos= []
+
+@app.get("/tasks")
+async def create(aluno: model.Aluno):
+    alunos.append(aluno)   
 
